6d6ee8aa55770716d0331ebe13dae3f4930450af
William Alexander Aguirre Antolínez <86641219+AguirreAntolinez@users.noreply.github.com>
2021-08-17 00:04:50 -0500

    Update analisisdiscriminante.R

--- analisisdiscriminante.R
+++ analisisdiscriminante.R
@@ -1,2 +1,9 @@
 # analisismultivariado
-En este repositorio se encuentran código en R para el desarrollo de técnicas estadísticas para el análisis multivariado.
+#En este repositorio se encuentran código en R para el desarrollo de técnicas estadísticas para el análisis multivariado.
+dismodel<-lda(df$factor~df$var1+df$var2+df$var3)
+dismodel
+predicdis<-predict(dismodel,df)$class
+predicdis
+df$predicdis<-predicdis
+sum(df$varfactor == df$predicdis)
+tea <- 1 - sum(df$varfactor == df$predicdis)/sum(complete.cases(df))
